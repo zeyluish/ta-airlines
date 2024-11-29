f19524293b37f9607ca877f19fef87574cae6682
integeek <ambre.giraudlanza@outlook.fr>
2024-11-29 11:37:01 +0100

    vol

--- src/main/java/org/example/TestAeroport.java
+++ src/main/java/org/example/TestAeroport.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+
+import static org.example.Vol.planifierVol;
+
+
 public class TestAeroport {
     public static void main(String[] args) {
+        ArrayList listeVol = new ArrayList();
         Avion avion1 = new Avion(78,"Airbus", 200);
         Avion avion2 = new Avion(92,"Boieng", 150);
 
@@ -9,7 +15,11 @@ public class TestAeroport {
         Vol vol2 = new Vol(2,"Oulu", "Paris","19","22",false);
         System.out.println(avion1.toString());
         avion1.affecterVol(vol1);
-        avion2.affecterVol(vol1);
+        avion2.affecterVol(vol2);
+        listeVol.add(vol1);
+        listeVol.add(vol2);
+
+        planifierVol(listeVol);
 
 
     }
